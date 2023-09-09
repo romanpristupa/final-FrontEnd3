8b5fbce4074c288bc8a523bf4b746f38f33f89bb
romanpristupa <romanalexis_16_@hotmail.com>
2023-09-09 17:40:30 -0300

    Segundo Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 import Home from "./Routes/Home";
 import Contact from "./Routes/Contact";
 import Favs from "./Routes/Favs";
@@ -10,10 +11,9 @@ import NotFound from "./Routes/NotFound";
 
 function App() {
   return (
-      <div className="App">
+      <div>
           <Router>
           <Navbar/>
-          <Footer/>
             <Routes>              
                 <Route index element={<Home/>}/>
                 <Route path="contact" element={<Contact/>} />
@@ -21,6 +21,7 @@ function App() {
                 <Route path="detail" element={<Detail/>} />
                 <Route path="*" element={<NotFound/>} />
             </Routes>
+          <Footer/>
           </Router>
       </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,11 @@
-import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
-
-  //   // Definir un estado para el tema actual
-  //   const [theme, setTheme] = useState('light');
-
-  //    // Función para cambiar el tema
-  // const toggleTheme = () => {
-  //   if (theme === 'light') {
-  //     setTheme('dark');
-  //   } else {
-  //     setTheme('light');
-  //   }
-  // };
-
-  //   // Estilo condicional basado en el tema
-  //   const navbarStyle = {
-  //     backgroundColor: theme === 'dark' ? 'black' : 'white',
-  //     color: theme === 'dark' ? 'white' : 'black',
-  //   };
-
+  
   return (
-    <nav>
+    <nav >
       <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/contact">Contacto</Link></li>
